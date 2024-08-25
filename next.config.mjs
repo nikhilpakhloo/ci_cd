46bdcbc4c02a412a67be354859f6cb5c78790a3e
nikhilpakhloo <nikhilpakhloo@gmail.com>
2024-08-25 15:03:00 +0530

    workflow added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jsonplaceholder.typicode.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
