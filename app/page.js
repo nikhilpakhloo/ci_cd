33b15da3af08b0ba6adbe83de8df58dbbc26d375
nikhilpakhloo <nikhilpakhloo@gmail.com>
2024-08-26 10:40:53 +0530

    some changes added

--- app/page.js
+++ app/page.js
@@ -1,17 +1,11 @@
-import React from 'react'
-import Button from './_components/Button'
+import React from "react";
+import Button from "./_components/Button";
 
 export default function Home() {
   return (
-    <div className='flex justify-center h-screen items-center'>
-
-
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
+    <div className="flex flex-col justify-center h-screen items-center">
+      <h1>Hello Button</h1>
+      <Button />
     </div>
-  )
+  );
 }
