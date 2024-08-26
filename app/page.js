5deacf85ef16bb3d3990f2ae2a8415d524d97fa1
nikhilpakhloo <nikhilpakhloo@gmail.com>
2024-08-26 14:14:54 +0530

    Swc removed

--- app/page.js
+++ app/page.js
@@ -4,13 +4,14 @@ import Button from "./_components/Button";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center h-screen items-center">
-      <h1>Hello Button</h1>
+      <h1>Hello Nikhil Pakhloo</h1>
       <div className="flex gap-10 items-center">
         <Button />
         <Button />
       </div>
 
       <h1>How are you?</h1>
+      
     </div>
   );
 }
